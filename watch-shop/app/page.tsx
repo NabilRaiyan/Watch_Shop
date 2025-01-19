8b1e99948dcc359effc2fea59e4210eb8565df93
NabilRaiyan <rraiyan77@gmail.com>
2025-01-19 19:15:07 +0600

    creating project

--- watch-shop/app/page.tsx
+++ watch-shop/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <h1>Hello world</h1>
       <h1>Hello world</h1>
+      <h1>Hello world</h1>
+
 
       </main>
      
