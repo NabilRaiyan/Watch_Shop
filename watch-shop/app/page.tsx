9456aee75319d073fe7f89856be680a5c23409d8
NabilRaiyan <rraiyan77@gmail.com>
2025-01-30 21:55:19 +0600

    creating project

--- watch-shop/app/page.tsx
+++ watch-shop/app/page.tsx
@@ -9,10 +9,7 @@ export default function Home() {
         <h1>Hello world</h1>
         <h1>Hello world</h1>
         <h1>Hello world</h1>
-        <h1>Hello world</h1>
-        <h1>Hello world</h1>
-        <h1>Hello world</h1>
-        <h1>Hello world</h1>
+        
 
       </main>
      
