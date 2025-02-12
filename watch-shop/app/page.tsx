76d1b0424c5c3d742a4216a30ef971598cd95596
NabilRaiyan <rraiyan77@gmail.com>
2025-02-12 22:05:46 +0600

    creaitng project

--- watch-shop/app/page.tsx
+++ watch-shop/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
         <h1>Hello world</h1>
         <h1>Hello world</h1>
         <h1>Hello world</h1>
-        <h1>Hello world</h1>
       </main>
      
     </div>
